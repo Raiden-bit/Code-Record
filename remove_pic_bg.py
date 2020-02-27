e0511acbf262b35f7703e6f1bcc8f078e0bf1719
Raiden-bit <59695988+Raiden-bit@users.noreply.github.com>
2020-02-27 21:08:40 +0800

    Update remove_pic_bg.py

--- remove_pic_bg.py
+++ remove_pic_bg.py
@@ -10,7 +10,7 @@ def remove_pic(key,pic_list):
         rmbg.remove_background_from_img_file("D:/removebg/temp_pic/" + each_pic,bg_color= 'white')
 def resize_pic(pic_blist,pic_alist):
     for each_pic in pic_alist:
-        if len(pic_alist) == len(pic_blist):
+        if len(pic_alist) == len(pic_blist):        
             print('No pic need resize')
             break
         if each_pic not in pic_blist:
@@ -18,11 +18,9 @@ def resize_pic(pic_blist,pic_alist):
             r_pic = Image.open("D:/removebg/temp_pic/" + each_pic)
             r_pic = r_pic.resize((800, 800))
             r_pic.save("D:/removebg/resize_pic/" + each_pic)
-
-
 if __name__ == '__main__':
-    pic_b = get_pic_names("D:/removebg/temp_pic")
-    app_key = input("Please input the app_key>>>")
+    pic_b = get_pic_names("D:/removebg/temp_pic")      
+    app_key = input("Please input the app_key>>>")                    #apply api key from removebg
     print("Loading,removing pic background!!!")
     remove_pic(app_key,pic_b)
     print("Done!!!")
