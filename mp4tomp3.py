81407543f7ec0a6907b2e7c1771a1e02175f5bfb
Raiden-bit <59695988+Raiden-bit@users.noreply.github.com>
2020-02-27 11:28:06 +0800

    Create mp4tomp3.py

---
+++ mp4tomp3.py
@@ -0,0 +1,18 @@
+from moviepy.editor import VideoFileClip
+import os
+def get_movie(dir):                                                       #get mp4 dir
+    list_movie = os.listdir(dir)
+    return list_movie
+movie_list = get_movie('D:/mp4tomp3')
+#print(movie_list)
+for each_movie in movie_list:
+    if '.' in each_movie:                                                   #only get mp4 file
+        movie_name = each_movie.split('.')[0]
+        video = VideoFileClip(f'D:/mp4tomp3/{each_movie}')
+        c_audio = video.audio                                                #mp3 audio
+        c_audio.write_audiofile(f'D:/mp4tomp3/mp3/{movie_name}.mp3')
+    else:
+        continue
+
+
+
